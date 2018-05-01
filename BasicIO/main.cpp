9d1aaf4be8b6d214ece9b0b3f698c6a8c09db10e
Slone <jbslon01@louisville.edu>
2018-05-01 12:40:12 -0400

    Daily post

--- BasicIO/main.cpp
+++ BasicIO/main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
+#include <vector>
 
-using namespace std;
+// Just trying to remember stuff.
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int a, b;
+
+    using std::cout;
+    using std::cin;
+    using std::endl;
+    using std::vector;
+
+    cout << "Enter two integers, seperated by a space: ";
+    cin >> a >> b;
+    cout << "You Entered: " << a << " " << b << endl;
+
+    int n;
+    cout << "Play with a pseudo-variable number of data items." << endl;
+    cout << "How many variables? ";
+    cin >> n;
+
+    vector<int> v;
+    int input;
+
+    for(int i = 0; i < n; i++) {
+        cin >> input;
+        v.push_back(input);
+    }
+
+    cout << "You Entered: " << endl;
+    for(int i = 0; i < n; i++) {
+        cout << v[i] << endl;
+    }
+
+
     return 0;
 }
